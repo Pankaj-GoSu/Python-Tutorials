559d6aa6674c5e379f325a4c00a21e92c65a71a3
Pankaj(GoSu) <Pankajgoswami330@gmail.com>
2021-07-28 15:49:22 +0530

    Added NumPy files

---
+++ NumPy/Intro_NumPy.py
@@ -0,0 +1,11 @@
+#========== NumPy =========
+
+'''
+# Why use numpy?
+-> NumPy provides efficient storage.
+-> It also provides better ways of handling data for processing.
+-> It is fast.
+-> NumPy uses relatively less memory to store data.
+
+'''
+
