1c160ae32371bab83163830be49d8c240508d2b4
Pankaj(GoSu) <Pankajgoswami330@gmail.com>
2021-07-28 17:06:48 +0530

    added some comment and code

--- NumPy/Intro_NumPy.py
+++ NumPy/Intro_NumPy.py
@@ -1,6 +1,14 @@
 #========== NumPy =========
 
 '''
+# NumPy stands for " Numerical Python "
+
+--> Fundamental python package for scientific computing.
+--> Tools and technique to solve mathematical problems.
+
+'''
+
+'''
 # Why use numpy?
 -> NumPy provides efficient storage.
 -> It also provides better ways of handling data for processing.
