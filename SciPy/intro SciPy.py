62ea1726c6d257e95fbc1bad7ce5cb515639a121
Pankaj(GoSu) <Pankajgoswami330@gmail.com>
2021-07-30 12:46:28 +0530

    Added Scipy tuts

---
+++ SciPy/intro SciPy.py
@@ -0,0 +1,10 @@
+#======== This Tutorial is for SciPy library in Python.===========
+
+'''
+# SciPy ==> Scientific Python
+Scipy is a collection of mathematical algorithms and concenience functions built on the 
+NumPy extentsion of python.
+
+'''
+
+ 
