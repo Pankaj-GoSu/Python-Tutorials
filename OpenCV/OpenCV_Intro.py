5a920a525965f6ae75814c9025a83c8977d80888
Pankaj(GoSu) <Pankajgoswami330@gmail.com>
2021-08-04 08:41:39 +0530

    Added intro of OpenCV

---
+++ OpenCV/OpenCV_Intro.py
@@ -0,0 +1,9 @@
+#=========== Introduction of OpenCV Tuttorial in Python =================
+
+'''
+# What is OpenCV
+-> OpenCV(Open Source Computer Vision Library) ia an open source computer vision and 
+machine learning software library.
+-> The library has more than 2500 optimized algorithms.
+
+'''
